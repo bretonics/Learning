b3669fbc7e4dc59c787eac730ab515af8eebaeee
Andres Breton <bretonics@users.noreply.github.com>
2015-08-01 17:28:06 -0400

    add file

--- Python/FoorLoops/chap8ex2.py
+++ Python/FoorLoops/chap8ex2.py
@@ -13,7 +13,7 @@ import sys
 # loop through grades file -> create histogram
 bar = "" #holds asterix
 for grade in open("../grades.txt"):
-    for i in range(1, int(grade)+1): #go through all grades
+    for i in range(1, int(grade)+1): #go through all numbers up until grade value
         if i % 5 == 0: #if i is evenly diveded by 5
             bar = bar+"*"
     print(bar, i)
